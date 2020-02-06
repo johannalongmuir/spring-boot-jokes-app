caecca7e541465f007ca1e9f7ef64cef43cd54c9
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-06 14:41:48 +0000

    Add Java Configuration Bean for imported Chuck Norris Quote List

---
+++ src/main/java/com/longmuir/springbootjokesapp/config/ChuckNorrisConfiguration.java
@@ -0,0 +1,15 @@
+package com.longmuir.springbootjokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+}
