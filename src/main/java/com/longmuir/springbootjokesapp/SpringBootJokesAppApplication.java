b28d9e4913b55d0a011342b0ca94511e982d9a7b
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-06 10:31:26 +0000

    Initial commit

---
+++ src/main/java/com/longmuir/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.longmuir.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokesAppApplication.class, args);
+	}
+
+}
