7c380fce88b23cbff520da23a4bbf79832fe09d7
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-06 11:08:59 +0000

    Closes #3. Closes #4. Closes #5. Closes #6. Add MVC

--- src/main/java/com/longmuir/springbootjokesapp/controllers/JokeController.java
+++ src/main/java/com/longmuir/springbootjokesapp/controllers/JokeController.java
@@ -1,4 +1,26 @@
 package com.longmuir.springbootjokesapp.controllers;
 
+import com.longmuir.springbootjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(@Qualifier("jokeServiceImpl") JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+
 }
